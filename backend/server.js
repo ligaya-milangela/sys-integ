b54e190d08c5a487e6e35ae35e1af06c1d04f9bf
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2025-05-05 19:29:23 +0800

    url added

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://sys-integ.onrender.com'],
   credentials: true,
 }));
 
