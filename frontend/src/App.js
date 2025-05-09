b3220ea2760829ac97804e9b0168221aa179f71d
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2025-05-10 02:21:50 +0800

    private route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import GoogleMeetButton from './components/GoogleMeetButton';
 import MeetingScreen from './pages/meetings/meeting_screen';
 import MinuteNoteEdit from './pages/meetings/minute_note_edit';
@@ -10,11 +10,13 @@ import AttendanceForm from './pages/attendance/attendance_form';
 import AttendanceList from './pages/attendance/attendance_list';
 import AttendanceDetails from './pages/attendance/attendance_details';
 import Login from './pages/auth/login';
-import Signup from './pages/auth/signup'; 
+import Signup from './pages/auth/signup';
+import PrivateRoute from './components/PrivateRoute';
+
 function HomePage() {
   return (
     <div>
-      <Login/>
+      <Login />
     </div>
   );
 }
@@ -25,18 +27,90 @@ function App() {
       <div style={{ flex: 1, padding: '20px', marginLeft: '200px' }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/meeting_screen" element={<MeetingScreen />} />
-          <Route path="/minute_note_detail/:id" element={<MinuteNoteDetail />} />
-          <Route path="/minute_view_detail/:id" element={<MinuteNoteView />} />
-          <Route path="/minute_note_edit/:id" element={<MinuteNoteEdit />} />
-          <Route path="/minute_create" element={<MinuteCreate />} />
-          <Route path="/attendance" element={<AttendanceList />} />
-          <Route path="/attendance/details/:id" element={<AttendanceDetails />} />
-          <Route path="/attendance/add" element={<AttendanceForm />} />
-          <Route path="/attendance/create" element={<AttendanceForm />} />
-          <Route path="/attendance/list" element={<AttendanceList />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="/meeting_screen"
+            element={
+              <PrivateRoute>
+                <MeetingScreen />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/minute_note_detail/:id"
+            element={
+              <PrivateRoute>
+                <MinuteNoteDetail />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/minute_view_detail/:id"
+            element={
+              <PrivateRoute>
+                <MinuteNoteView />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/minute_note_edit/:id"
+            element={
+              <PrivateRoute>
+                <MinuteNoteEdit />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/minute_create"
+            element={
+              <PrivateRoute>
+                <MinuteCreate />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/attendance"
+            element={
+              <PrivateRoute>
+                <AttendanceList />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/attendance/details/:id"
+            element={
+              <PrivateRoute>
+                <AttendanceDetails />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/attendance/add"
+            element={
+              <PrivateRoute>
+                <AttendanceForm />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/attendance/create"
+            element={
+              <PrivateRoute>
+                <AttendanceForm />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/attendance/list"
+            element={
+              <PrivateRoute>
+                <AttendanceList />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
     </div>
