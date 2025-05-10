46551967b1dd60eb4f6d9d124b07ef336f6c28a6
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2025-05-11 00:38:02 +0800

    inangyan

--- backend/server.js
+++ backend/server.js
@@ -12,23 +12,14 @@ const app = express();
 console.log(process.env.REACT_APP_API_URL);
 
 // Middleware
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://sys-integ-backend.onrender.com', 'https://attendance-and-notes-system.netlify.app', 'https://next-auro.vercel.app'],
+  credentials: true,
+}));
 
-const corsOptions = {
-  origin: [
-    'http://localhost:3000',
-    'https://attendance-and-notes-system.netlify.app',
-    'https://sys-integ-backend.onrender.com',
-    'https://next-auro.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-};
+app.use(express.json());
 
-app.use(cors(corsOptions));
-
-app.options('*', cors(corsOptions));
-
+// Session handling
 app.use(session({
   name: 'session',
   keys: [process.env.SESSION_SECRET || 'default_key'],
